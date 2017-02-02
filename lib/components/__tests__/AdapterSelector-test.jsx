0e33f01173e27b6623d9b554c6edf73dcb5eef8f
Magne Rodem <magne@rodem.no>
2017-02-02 16:35:33 +0100

    Set up testing with jest

---
+++ lib/components/__tests__/AdapterSelector-test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import AdapterSelector from '../AdapterSelector';
+
+describe('AdapterSelector', () => {
+    it('should render correctly', () => {
+        const tree = renderer.create(
+            <AdapterSelector />
+        ).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+});
