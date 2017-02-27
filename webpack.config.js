a80b04dcf3bc1f6e42ee5405a4972a2912477897
Magne Rodem <magne@rodem.no>
2017-02-27 14:25:13 +0100

    Set NODE_ENV while keeping the rest of the variables
    When NODE_ENV was set by webpack, it set:
    
    process.env = { NODE_ENV: 'development' }
    
    We want to keep the existing properties on process.env, so now
    only setting:
    
    process.env.NODE_ENV = 'development'
    
    The process.env.windir variable used in lib/api/registry/reg.js was
    undefined because of this.

--- webpack.config.js
+++ webpack.config.js
@@ -51,9 +51,7 @@ module.exports = {
     },
     plugins: [
         new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: JSON.stringify(nodeEnv),
-            },
+            'process.env.NODE_ENV': JSON.stringify(nodeEnv),
         }),
     ],
     externals: createExternals(),
