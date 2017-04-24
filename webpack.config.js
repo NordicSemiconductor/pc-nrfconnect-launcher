a223c4c2bbd3ce4b4f5d14bc391114a0ed15cbca
bencefr <bence.frenyo@nordicsemi.no>
2017-04-24 08:09:52 +0200

    removed unused raw loader

--- webpack.config.js
+++ webpack.config.js
@@ -47,9 +47,6 @@ module.exports = {
         }, {
             test: /\.(png|gif|ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
             loader: 'file-loader',
-        }, {
-            test: /\.hex$/,
-            loader: 'raw-loader',
         }],
     },
     resolve: {
