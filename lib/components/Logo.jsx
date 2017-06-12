59546b0397a4ca9794ce66e14109eeb3bc683275
bencefr <bence.frenyo@nordicsemi.no>
2017-06-12 15:04:11 +0200

    Use the proper function to open url in browser.

--- lib/components/Logo.jsx
+++ lib/components/Logo.jsx
@@ -1,35 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import logo from '../../resources/nordiclogo_neg.png';
+import { openUrlInDefaultBrowser } from '../util/fileUtil';
 
 const Logo = ({
     src,
-    href,
     alt,
     cssClass,
     containerCssClass,
+    onClick,
 }) => (
-    <div className={containerCssClass}>
-        <a href={href} target="_blank" rel="noopener noreferrer">
-            <img className={cssClass} src={src} alt={alt} />
-        </a>
+    <div
+        className={containerCssClass}
+        role="link"
+        onClick={onClick}
+        tabIndex="0"
+    >
+        <img className={cssClass} src={src} alt={alt} />
     </div>
 );
 
 Logo.propTypes = {
     src: PropTypes.string,
-    href: PropTypes.string,
     alt: PropTypes.string,
     cssClass: PropTypes.string,
     containerCssClass: PropTypes.string,
+    onClick: PropTypes.function,
 };
 
 Logo.defaultProps = {
     src: logo,
-    href: 'http://www.nordicsemi.com/nRFConnect',
     alt: 'nRF Connect',
     cssClass: 'core-logo',
     containerCssClass: 'core-logo-container',
+    onClick: openUrlInDefaultBrowser.bind(null, 'http://www.nordicsemi.com/nRFConnect'),
 };
 
 export default Logo;
