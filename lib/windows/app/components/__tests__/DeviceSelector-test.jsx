b01f7be9d07bd850e06634d4a9105f36d81e999e
Magne Rodem <magne@rodem.no>
2018-03-08 12:42:46 +0100

    Update/add some simple tests of the DeviceSelector
    The tests were failing. Removed many of the old tests we had, because
    the component is a lot more simple now. Kept a few tests just to verify
    that it renders closed, expanded, and with one device.

--- lib/windows/app/components/__tests__/DeviceSelector-test.jsx
+++ lib/windows/app/components/__tests__/DeviceSelector-test.jsx
@@ -48,15 +48,19 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import DeviceSelector from '../DeviceSelector';
 
+const selectDeviceText = 'Select device';
+
 describe('DeviceSelector', () => {
     it('should render empty device list, not expanded', () => {
         expect(renderer.create(
             <DeviceSelector
                 devices={[]}
-                toggleExpanded={() => {}}
+                isExpanded={false}
+                togglerText={selectDeviceText}
+                onToggle={() => {}}
+                displayCloseItem={false}
                 onSelect={() => {}}
                 onDeselect={() => {}}
-                bindHotkey={() => {}}
             />,
         )).toMatchSnapshot();
     });
@@ -66,310 +70,37 @@ describe('DeviceSelector', () => {
             <DeviceSelector
                 devices={[]}
                 isExpanded
-                toggleExpanded={() => {}}
+                togglerText={selectDeviceText}
+                onToggle={() => {}}
+                displayCloseItem={false}
                 onSelect={() => {}}
                 onDeselect={() => {}}
-                bindHotkey={() => {}}
             />,
         )).toMatchSnapshot();
     });
 
-    it('should render two devices', () => {
+    it('should render one device with serialport, usb, and jlink traits', () => {
         expect(renderer.create(
             <DeviceSelector
                 devices={[
                     {
-                        busNumber: 1,
-                        deviceAddress: 1,
-                        comName: '/dev/tty0',
-                        vendorId: 0x1366,
-                        serialNumber: '123456',
-                    }, {
-                        busNumber: 1,
-                        deviceAddress: 2,
-                        comName: '/dev/tty1',
-                        vendorId: 0x1366,
-                        serialNumber: '456789',
+                        serialNumber: '123456789',
+                        serialport: {
+                            comName: '/dev/ttyACM0',
+                        },
+                        usb: {
+                            manufacturer: 'Nordic Semiconductor',
+                            product: 'nRF52 USB',
+                        },
+                        jlink: {},
                     },
                 ]}
                 isExpanded
-                toggleExpanded={() => {}}
+                togglerText={selectDeviceText}
+                onToggle={() => {}}
+                displayCloseItem={false}
                 onSelect={() => {}}
                 onDeselect={() => {}}
-                bindHotkey={() => {}}
-            />,
-        )).toMatchSnapshot();
-    });
-
-    it('should render only devices that have a serial number by default', () => {
-        expect(renderer.create(
-            <DeviceSelector
-                devices={[
-                    {
-                        busNumber: 1,
-                        deviceAddress: 1,
-                        comName: '/dev/tty0',
-                        vendorId: 0x1366,
-                        serialNumber: '123456',
-                    }, {
-                        busNumber: 1,
-                        deviceAddress: 2,
-                        comName: '/dev/tty1',
-                        vendorId: 0x1366,
-                    },
-                ]}
-                isExpanded
-                toggleExpanded={() => {}}
-                onSelect={() => {}}
-                onDeselect={() => {}}
-                bindHotkey={() => {}}
-            />,
-        )).toMatchSnapshot();
-    });
-
-    it('should render only Nordic Semiconductor and Segger devices by default', () => {
-        expect(renderer.create(
-            <DeviceSelector
-                devices={[
-                    {
-                        busNumber: 1,
-                        deviceAddress: 1,
-                        comName: '/dev/tty0',
-                        vendorId: 0x1915,
-                        serialNumber: '123456',
-                    }, {
-                        busNumber: 1,
-                        deviceAddress: 2,
-                        comName: '/dev/tty1',
-                        serialNumber: '456789',
-                        vendorId: 0x1366,
-                    }, {
-                        busNumber: 1,
-                        deviceAddress: 3,
-                        comName: '/dev/tty2',
-                        serialNumber: '345678',
-                        vendorId: 0x1337,
-                    },
-                ]}
-                isExpanded
-                toggleExpanded={() => {}}
-                onSelect={() => {}}
-                onDeselect={() => {}}
-                bindHotkey={() => {}}
-            />,
-        )).toMatchSnapshot();
-    });
-
-    it('should render devices with custom filter', () => {
-        expect(renderer.create(
-            <DeviceSelector
-                devices={[
-                    {
-                        busNumber: 1,
-                        deviceAddress: 1,
-                        comName: '/dev/tty0',
-                        serialNumber: '123456',
-                        vendorId: 0x1366,
-                    }, {
-                        busNumber: 1,
-                        deviceAddress: 2,
-                        serialNumber: '456789',
-                        vendorId: 0x1366,
-                    },
-                ]}
-                isExpanded
-                toggleExpanded={() => {}}
-                onSelect={() => {}}
-                onDeselect={() => {}}
-                bindHotkey={() => {}}
-                filter={device => !!device.comName}
-            />,
-        )).toMatchSnapshot();
-    });
-
-    it('should order devices by serial number by default', () => {
-        expect(renderer.create(
-            <DeviceSelector
-                devices={[
-                    {
-                        busNumber: 1,
-                        deviceAddress: 1,
-                        comName: '/dev/tty0',
-                        vendorId: 0x1366,
-                        serialNumber: '456789',
-                    }, {
-                        busNumber: 1,
-                        deviceAddress: 2,
-                        vendorId: 0x1366,
-                        serialNumber: '123456',
-                    }, {
-                        busNumber: 1,
-                        deviceAddress: 3,
-                        vendorId: 0x1366,
-                        serialNumber: '678901',
-                    },
-                ]}
-                isExpanded
-                toggleExpanded={() => {}}
-                onSelect={() => {}}
-                onDeselect={() => {}}
-                bindHotkey={() => {}}
-            />,
-        )).toMatchSnapshot();
-    });
-
-    it('should order devices by custom order', () => {
-        expect(renderer.create(
-            <DeviceSelector
-                devices={[
-                    {
-                        busNumber: 1,
-                        deviceAddress: 1,
-                        comName: '/dev/tty3',
-                        vendorId: 0x1366,
-                        serialNumber: '456789',
-                    }, {
-                        busNumber: 1,
-                        deviceAddress: 2,
-                        comName: '/dev/tty1',
-                        vendorId: 0x1366,
-                        serialNumber: '123456',
-                    }, {
-                        busNumber: 1,
-                        deviceAddress: 3,
-                        comName: '/dev/tty2',
-                        vendorId: 0x1366,
-                        serialNumber: '678901',
-                    },
-                ]}
-                isExpanded
-                toggleExpanded={() => {}}
-                onSelect={() => {}}
-                onDeselect={() => {}}
-                bindHotkey={() => {}}
-                compareFunction={(deviceA, deviceB) => {
-                    if (deviceA.comName < deviceB.comName) {
-                        return -1;
-                    }
-                    if (deviceA.comName > deviceB.comName) {
-                        return 1;
-                    }
-                    return 0;
-                }}
-            />,
-        )).toMatchSnapshot();
-    });
-
-    it('should render devices with custom device details', () => {
-        expect(renderer.create(
-            <DeviceSelector
-                devices={[
-                    {
-                        busNumber: 1,
-                        deviceAddress: 1,
-                        comName: '/dev/tty0',
-                        vendorId: 0x1366,
-                        serialNumber: '123456',
-                        manufacturer: 'SEGGER',
-                    }, {
-                        busNumber: 1,
-                        deviceAddress: 2,
-                        serialNumber: '456789',
-                        vendorId: 0x1915,
-                        manufacturer: 'Nordic Semiconductor',
-                    },
-                ]}
-                isExpanded
-                toggleExpanded={() => {}}
-                onSelect={() => {}}
-                onDeselect={() => {}}
-                bindHotkey={() => {}}
-                deviceDetailsParser={device => device.manufacturer}
-            />,
-        )).toMatchSnapshot();
-    });
-
-    it('should render two devices, one selected', () => {
-        expect(renderer.create(
-            <DeviceSelector
-                devices={[
-                    {
-                        busNumber: 1,
-                        deviceAddress: 1,
-                        comName: '/dev/tty0',
-                        vendorId: 0x1366,
-                        serialNumber: '123456',
-                    }, {
-                        busNumber: 1,
-                        deviceAddress: 2,
-                        vendorId: 0x1366,
-                        comName: '/dev/tty1',
-                        serialNumber: '456789',
-                    },
-                ]}
-                selectedDevice={{
-                    busNumber: 1,
-                    deviceAddress: 2,
-                    vendorId: 0x1366,
-                    comName: '/dev/tty1',
-                    serialNumber: '456789',
-                }}
-                isExpanded
-                toggleExpanded={() => {}}
-                onSelect={() => {}}
-                onDeselect={() => {}}
-                bindHotkey={() => {}}
-            />,
-        )).toMatchSnapshot();
-    });
-
-    it('should render empty list while loading', () => {
-        expect(renderer.create(
-            <DeviceSelector
-                devices={[
-                    {
-                        busNumber: 1,
-                        deviceAddress: 1,
-                        comName: '/dev/tty0',
-                        vendorId: 0x1366,
-                        serialNumber: '123456',
-                    },
-                ]}
-                isExpanded
-                isLoading
-                toggleExpanded={() => {}}
-                onSelect={() => {}}
-                onDeselect={() => {}}
-                bindHotkey={() => {}}
-            />,
-        )).toMatchSnapshot();
-    });
-
-    it('should render empty list while loading and device is selected', () => {
-        expect(renderer.create(
-            <DeviceSelector
-                devices={[
-                    {
-                        busNumber: 1,
-                        deviceAddress: 1,
-                        comName: '/dev/tty0',
-                        vendorId: 0x1366,
-                        serialNumber: '123456',
-                    },
-                ]}
-                selectedDevice={{
-                    busNumber: 1,
-                    deviceAddress: 1,
-                    comName: '/dev/tty0',
-                    vendorId: 0x1366,
-                    serialNumber: '123456',
-                }}
-                isExpanded
-                isLoading
-                toggleExpanded={() => {}}
-                onSelect={() => {}}
-                onDeselect={() => {}}
-                bindHotkey={() => {}}
             />,
         )).toMatchSnapshot();
     });
