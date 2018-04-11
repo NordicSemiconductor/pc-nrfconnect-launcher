7a99ad73fd5181c116700136fc149c0e508b4054
Magne Rodem <magne@rodem.no>
2018-04-11 08:43:06 +0200

    Look for jlink trait instead of device property

--- lib/windows/app/components/DeviceSelector.jsx
+++ lib/windows/app/components/DeviceSelector.jsx
@@ -97,7 +97,7 @@ export default class DeviceSelector extends React.Component {
                     { device.usb ?
                         (<li>USB: {device.usb.manufacturer} {device.usb.product}</li>)
                         : null }
-                    { device.jlink ? (<li>Debug probe</li>) : null }
+                    { device.traits.includes('jlink') ? (<li>Debug probe</li>) : null }
                 </ul>
             </MenuItem>
         );
