376ad17d0b255f20a523b18a8152aee3429bf86d
Bence Frenyó <26139379+bencefr@users.noreply.github.com>
2019-08-20 12:59:39 +0200

    Fix app-item wrapping behaviour and initial size

--- lib/windows/launcher/components/AppItem.jsx
+++ lib/windows/launcher/components/AppItem.jsx
@@ -69,7 +69,7 @@ const AppItem = ({
                 <Col xs="auto my-2 mr-3" className="d-flex align-items-start">
                     <AppIcon app={app} />
                 </Col>
-                <Col className="text-width">
+                <Col>
                     <div className="h8">
                         {app.displayName || app.name}
                     </div>
@@ -82,7 +82,7 @@ const AppItem = ({
                         {upgradeAvailable && <> (v{app.latestVersion} available)</>}
                     </div>
                 </Col>
-                <Col xs="auto ml-auto" className="d-flex align-items-end my-3 pl-3">
+                <Col xs="auto ml-auto" className="d-flex align-items-center my-3 pl-3">
                     <ButtonToolbar className="wide-btns">
                         {upgradeAvailable && (
                             <Button
