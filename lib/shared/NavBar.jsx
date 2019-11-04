37de9fb81e3923bf4e510814b2259826720862a7
Marko Schulz <Marko.Schulz@nordicsemi.no>
2019-11-04 17:21:05 +0100

    Add new NavBar component

---
+++ lib/shared/NavBar.jsx
@@ -0,0 +1,80 @@
+/* Copyright (c) 2015 - 2017, Nordic Semiconductor ASA
+ *
+ * All rights reserved.
+ *
+ * Use in source and binary forms, redistribution in binary form only, with
+ * or without modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions in binary form, except as embedded into a Nordic
+ *    Semiconductor ASA integrated circuit in a product or a software update for
+ *    such product, must reproduce the above copyright notice, this list of
+ *    conditions and the following disclaimer in the documentation and/or other
+ *    materials provided with the distribution.
+ *
+ * 2. Neither the name of Nordic Semiconductor ASA nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * 3. This software, with or without modification, must only be used with a Nordic
+ *    Semiconductor ASA integrated circuit.
+ *
+ * 4. Any software provided in binary form under this license must not be reverse
+ *    engineered, decompiled, modified and/or disassembled.
+ *
+ * THIS SOFTWARE IS PROVIDED BY NORDIC SEMICONDUCTOR ASA "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL NORDIC SEMICONDUCTOR ASA OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import React from 'react';
+import { string, node } from 'prop-types';
+
+// FIXME: Correct the following
+// import DeviceSelectorContainer from '../containers/DeviceSelectorContainer';
+// import SerialPortSelectorContainer from '../containers/SerialPortSelectorContainer';
+// import NavMenuContainer from '../containers/NavMenuContainer';
+// import MainMenuContainer from '../containers/MainMenuContainer';
+
+import '../../resources/css/brand19/nav-bar.scss';
+import Logo from './Logo';
+
+const NavBarItem = ({ children }) => <div className="core19-nav-bar-item">{children}</div>;
+NavBarItem.propTypes = {
+    children: node.isRequired,
+};
+
+const NavBar = ({ deviceSelect, title }) => (
+    <div className="core19-nav-bar">
+        {/* <MainMenuContainer />
+        <div className="core-nav-section core-padded-row">
+            {
+                appConfig.selectorTraits
+                    ? <DeviceSelectorContainer traits={appConfig.selectorTraits} />
+                    : <SerialPortSelectorContainer />
+            }
+        </div>
+        <NavMenuContainer /> */}
+        <NavBarItem>{title}</NavBarItem>
+        <Logo />
+    </div>
+);
+
+NavBar.propTypes = {
+    deviceSelect: node,
+    title: string,
+};
+
+NavBar.defaultProps = ({
+    deviceSelect: '',
+    title: '',
+});
+
+export default NavBar;
