89a2f4c8bc65f88180e92f8a9ba3aa9a10eb369b
Marko Schulz <Marko.Schulz@nordicsemi.no>
2019-11-18 13:49:09 +0100

    Return etag and statusCode from downloadToBuffer
    Because we will need these when downloading only changed resources.

--- main/net.js
+++ main/net.js
@@ -68,19 +68,26 @@ function downloadToBuffer(url, headers = {}) {
         Object.keys(headers).forEach(key => request.setHeader(key, headers[key]));
 
         request.on('response', response => {
-            if (response.statusCode >= 400) {
-                const error = new Error(`Unable to download ${url}. Got status code `
-                    + `${response.statusCode}`);
-                error.statusCode = response.statusCode;
+            const { statusCode } = response;
+            if (statusCode >= 400) {
+                const error = new Error(`Unable to download ${url}. Got status code ${statusCode}`);
+                error.statusCode = statusCode;
                 reject(error);
                 return;
             }
+            const etag = Array.isArray(response.headers.etag)
+                ? response.headers.etag[0] : undefined;
+
             const buffer = [];
             const addToBuffer = data => {
                 buffer.push(data);
             };
             response.on('data', data => addToBuffer(data));
-            response.on('end', () => resolve(Buffer.concat(buffer)));
+            response.on('end', () => resolve({
+                buffer: Buffer.concat(buffer),
+                etag,
+                statusCode,
+            }));
             response.on('error', error => reject(new Error(`Error when reading ${url}: `
                 + `${error.message}`)));
         });
@@ -102,7 +109,7 @@ function downloadToBuffer(url, headers = {}) {
  */
 function downloadToString(url, headers) {
     return downloadToBuffer(url, headers)
-        .then(buffer => buffer.toString());
+        .then(({ buffer }) => buffer.toString());
 }
 
 /**
@@ -129,8 +136,8 @@ function downloadToJson(url, headers) {
 function downloadToFile(url, filePath) {
     return new Promise((resolve, reject) => {
         downloadToBuffer(url)
-            .then(data => {
-                fs.writeFile(filePath, data, err => {
+            .then(({ buffer }) => {
+                fs.writeFile(filePath, buffer, err => {
                     if (err) {
                         reject(err);
                     } else {
