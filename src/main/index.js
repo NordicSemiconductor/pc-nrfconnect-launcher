59a4ab929e80e5cf055928f08e985b7061bf6f8f
Chun Fan <4866953+chunfantasy@users.noreply.github.com>
2020-10-16 14:48:59 +0200

    Lower max memory size

--- src/main/index.js
+++ src/main/index.js
@@ -69,7 +69,7 @@ const handleDevtoolsRequest = require('./devtools');
 
 // Ensure that nRFConnect runs in a directory where it has permission to write
 process.chdir(electronApp.getPath('temp'));
-// electronApp.commandLine.appendSwitch('js-flags', '--max-old-space-size=4096');
+electronApp.commandLine.appendSwitch('js-flags', '--max-old-space-size=2048');
 
 config.init(argv);
 global.homeDir = config.getHomeDir();
