e66d571d6bf4b114db600682a5d6b8f5ec23f87f
Nicolai Berthelsen <nicolai.berthelsen@nordicsemi.no>
2021-09-22 14:33:13 +0200

    Update licenses

--- package.json.d.ts
+++ package.json.d.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+ */
+
 import { PackageJson } from 'pc-nrfconnect-shared';
 
 declare const packageJson: PackageJson;
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+ */
+
 const webpack = require('webpack');
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
