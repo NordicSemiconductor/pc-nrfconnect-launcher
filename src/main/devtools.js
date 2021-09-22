9bc7beabcdc58cae4bfff7e30ac09ed400999fed
Nicolai Berthelsen <nicolai.berthelsen@nordicsemi.no>
2021-09-22 13:42:28 +0200

    Handle missing devtools when bundling launcher

--- src/main/devtools.js
+++ src/main/devtools.js
@@ -38,11 +38,23 @@ const { app, session } = require('electron');
 const { exit } = require('process');
 const path = require('path');
 const fs = require('fs');
-const {
-    default: installExtension,
-    REACT_DEVELOPER_TOOLS,
-    REDUX_DEVTOOLS,
-} = require('electron-devtools-installer');
+
+let devToolsInstaller;
+let installExtension;
+let REACT_DEVELOPER_TOOLS;
+let REDUX_DEVTOOLS;
+
+try {
+    devToolsInstaller = require('electron-devtools-installer'); // eslint-disable-line global-require
+} catch (err) {
+    // Ignore missing devtools dependency here, check later for it when needed
+}
+
+if (devToolsInstaller) {
+    installExtension = devToolsInstaller.default;
+    REACT_DEVELOPER_TOOLS = devToolsInstaller.REACT_DEVELOPER_TOOLS;
+    REDUX_DEVTOOLS = devToolsInstaller.REDUX_DEVTOOLS;
+}
 
 const installDevtools = async () => {
     try {
@@ -58,32 +70,45 @@ const installDevtools = async () => {
 
 // Look for installed extensions and remove them
 const removeDevtools = () => {
-    [REDUX_DEVTOOLS.id, REACT_DEVELOPER_TOOLS.id].forEach(id => {
-        const extensionPath = path.join(
-            session.defaultSession.getStoragePath(),
-            'extensions',
-            id
-        );
+    try {
+        [REDUX_DEVTOOLS.id, REACT_DEVELOPER_TOOLS.id].forEach(id => {
+            const extensionPath = path.join(
+                session.defaultSession.getStoragePath(),
+                'extensions',
+                id
+            );
 
-        if (fs.existsSync(extensionPath)) {
-            fs.rmdirSync(extensionPath, { recursive: true });
-        }
-    });
-    exit(0);
+            if (fs.existsSync(extensionPath)) {
+                fs.rmdirSync(extensionPath, { recursive: true });
+            }
+        });
+        exit(0);
+    } catch (err) {
+        console.log('An error occurred while removing devtools: ', err);
+        exit(1);
+    }
 };
 
 // Look for installed extensions and load them
 const loadInstalledDevtools = () => {
-    const storagePath = session.defaultSession.getStoragePath();
-    [REDUX_DEVTOOLS.id, REACT_DEVELOPER_TOOLS.id].forEach(id => {
-        const extensionPath = path.join(storagePath, 'extensions', id);
+    try {
+        const storagePath = session.defaultSession.getStoragePath();
+        [REDUX_DEVTOOLS.id, REACT_DEVELOPER_TOOLS.id].forEach(id => {
+            const extensionPath = path.join(storagePath, 'extensions', id);
 
-        if (fs.existsSync(extensionPath)) {
-            session.defaultSession.loadExtension(extensionPath, {
-                allowFileAccess: true,
-            });
-        }
-    });
+            if (fs.existsSync(extensionPath)) {
+                session.defaultSession.loadExtension(extensionPath, {
+                    allowFileAccess: true,
+                });
+            }
+        });
+    } catch (err) {
+        console.log(
+            'An error occurred while loading installed devtools: ',
+            err
+        );
+        exit(1);
+    }
 };
 
 module.exports = () => {
