8a560e8de4a513d52643bbfcd50e3a725da7067c
Nicolai Berthelsen <nicolai.berthelsen@nordicsemi.no>
2021-10-05 12:56:40 +0200

    Remove tests from legacy section.
    They are not worth maintaing at this point as there is work ongoing to
    move all apps to new architecture.

--- src/legacy/api/jlink/__tests__/jlinkFacade-test.js
+++ src/legacy/api/jlink/__tests__/jlinkFacade-test.js
@@ -51,46 +51,6 @@ describe('JlinkFacade', () => {
             .then(() => expect(warningMessage).toContain('Lookup failed'));
     });
 
-    // it('returns empty map if comName is given, and two serial numbers found in registry, but none are connected', () => {
-    //     const facade = new JlinkFacade();
-    //     facade.registry.findJlinkIds = () =>
-    //         Promise.resolve(['000123456789', '000234567890']);
-    //     facade.nrfjprogjs.getSerialNumbers = callback =>
-    //         callback(null, [345678901]);
-    //     return facade
-    //         .getSerialNumberMap(['COM1'])
-    //         .then(map => expect(map.size).toEqual(0));
-    // });
-
-    // it('emits warning if comName is given, and two serial numbers found in registry, but none are connected', () => {
-    //     const facade = new JlinkFacade();
-    //     facade.registry.findJlinkIds = () =>
-    //         Promise.resolve(['000123456789', '000234567890']);
-    //     facade.nrfjprogjs.getSerialNumbers = callback =>
-    //         callback(null, [345678901]);
-    //     let warningMessage;
-    //     facade.on('warn', message => {
-    //         warningMessage = message;
-    //     });
-    //     return facade.getSerialNumberMap(['COM1']).then(() => {
-    //         expect(warningMessage).toEqual(
-    //             'Found serial numbers 000123456789, 000234567890 for ' +
-    //                 'COM1 in registry, but none of these are connected. Unable to identify serial number.'
-    //         );
-    //     });
-    // });
-
-    // it('rejects if comName is given, and two serial numbers found in registry, but connected devices lookup fails', () => {
-    //     const facade = new JlinkFacade();
-    //     facade.registry.findJlinkIds = () =>
-    //         Promise.resolve(['000123456789', '000234567890']);
-    //     facade.nrfjprogjs.getSerialNumbers = callback =>
-    //         callback(new Error('Lookup failed'));
-    //     return facade
-    //         .getSerialNumberMap(['COM1'])
-    //         .catch(error => expect(error.message).toContain('Lookup failed'));
-    // });
-
     it('returns map with serial number if comName is given, and one serial number found in registry', () => {
         const facade = new JlinkFacade();
         facade.registry.findJlinkIds = () => Promise.resolve(['000123456789']);
@@ -111,61 +71,4 @@ describe('JlinkFacade', () => {
             expect(warningMessage).not.toBeDefined();
         });
     });
-
-    // it('does not call nrfjprogjs if comName is given, and one serial number found in registry', () => {
-    //     const facade = new JlinkFacade();
-    //     facade.registry.findJlinkIds = () => Promise.resolve(['000123456789']);
-    //     facade.nrfjprogjs.getSerialNumbers = jest.fn();
-    //     return facade.getSerialNumberMap(['COM1']).then(() => {
-    //         expect(facade.nrfjprogjs.getSerialNumbers).not.toHaveBeenCalled();
-    //     });
-    // });
-
-    // it('returns map with serial number if comName is given, and two serial numbers found in registry, and one is connected', () => {
-    //     const facade = new JlinkFacade();
-    //     facade.registry.findJlinkIds = () =>
-    //         Promise.resolve(['000123456789', '000987654321']);
-    //     facade.nrfjprogjs.getSerialNumbers = callback =>
-    //         callback(null, [234567890, 123456789]);
-    //     return facade.getSerialNumberMap(['COM1']).then(map => {
-    //         expect(map.size).toEqual(1);
-    //         expect(map.get('COM1')).toEqual('000123456789');
-    //     });
-    // });
-
-    // it('emits warning if comName is given, and two serial numbers found in registry, and one is connected', () => {
-    //     const facade = new JlinkFacade();
-    //     facade.registry.findJlinkIds = () =>
-    //         Promise.resolve(['000123456789', '000987654321']);
-    //     facade.nrfjprogjs.getSerialNumbers = callback =>
-    //         callback(null, [234567890, 123456789]);
-    //     let warningMessage;
-    //     facade.on('warn', message => {
-    //         warningMessage = message;
-    //     });
-    //     return facade.getSerialNumberMap(['COM1']).then(() => {
-    //         expect(warningMessage).toEqual(
-    //             'Found serial numbers 000123456789, 000987654321 for ' +
-    //                 'COM1 in registry. 000123456789 is connected, so using that.'
-    //         );
-    //     });
-    // });
-
-    // it('returns map with serial numbers if multiple comNames are given, and two serial numbers found in registry for each, and one is connected for each', () => {
-    //     const facade = new JlinkFacade();
-    //     facade.registry.findJlinkIds = comName => {
-    //         const serialNumbers = {
-    //             COM1: ['000123456789', '000987654321'],
-    //             COM2: ['000456789012', '000345678901'],
-    //         };
-    //         return Promise.resolve(serialNumbers[comName]);
-    //     };
-    //     facade.nrfjprogjs.getSerialNumbers = callback =>
-    //         callback(null, [345678901, 234567890, 123456789]);
-    //     return facade.getSerialNumberMap(['COM1', 'COM2']).then(map => {
-    //         expect(map.size).toEqual(2);
-    //         expect(map.get('COM1')).toEqual('000123456789');
-    //         expect(map.get('COM2')).toEqual('000345678901');
-    //     });
-    // });
 });
