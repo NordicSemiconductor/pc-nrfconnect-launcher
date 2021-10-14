ab71a52d418ab675f7f4b1ba4770bb47fa2bba53
Nicolai Berthelsen <nicolai.berthelsen@nordicsemi.no>
2021-10-14 07:59:17 +0200

    Revert "Replace `loadURL` with `loadFile`"
    This reverts commit 96eb48e9567b5878099ae877053c8c3e7a5105f1.

--- src/main/browser.js
+++ src/main/browser.js
@@ -50,11 +50,7 @@ function createWindow(options) {
         splashScreen = createSplashScreen();
     }
 
-    if (options.filePath) {
-        browserWindow.loadFile(options.filePath);
-    } else {
-        browserWindow.loadURL(options.url);
-    }
+    browserWindow.loadURL(options.url);
 
     // Never navigate away from the given url, e.g. when the
     // user drags and drops a file into the browser window.
