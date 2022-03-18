75e3f0487e6ac2d07e223aa2d187b4134ca0e119
Marko Schulz <Marko.Schulz@nordicsemi.no>
2022-03-18 14:41:51 +0100

    Cleanup notarize.js
    - async/await was not needed
    - Use standard formatting

--- build/notarize.js
+++ build/notarize.js
@@ -8,14 +8,12 @@ require('dotenv').config();
 const { notarize } = require('electron-notarize');
 const pkgJson = require('../package.json');
 
-exports.default = async ({ electronPlatformName, appOutDir }) => (
-  (electronPlatformName === 'darwin')
-  ? await notarize({
-    appBundleId: pkgJson.build.appId,
-    appPath: `${appOutDir}/${pkgJson.build.productName}.app`,
-    appleId: process.env.APPLEID,
-    appleIdPassword: process.env.APPLEIDPASS,
-  })
-  : undefined
-);
-
+exports.default = ({ electronPlatformName, appOutDir }) =>
+    electronPlatformName === 'darwin'
+        ? notarize({
+              appBundleId: pkgJson.build.appId,
+              appPath: `${appOutDir}/${pkgJson.build.productName}.app`,
+              appleId: process.env.APPLEID,
+              appleIdPassword: process.env.APPLEIDPASS,
+          })
+        : undefined;
