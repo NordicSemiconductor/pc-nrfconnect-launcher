d14dab02293ec5dc3e67722fc7ccec35a66bbf8d
Marko Schulz <Marko.Schulz@nordicsemi.no>
2022-08-11 13:04:35 +0200

    Move esbuild config to own file

---
+++ esbuild.js
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+ */
+
+require('esbuild')
+    .build({
+        bundle: true,
+        color: true,
+        entryPoints: ['src/main/index.js'],
+        external: ['electron', './node_modules/*'],
+        logLevel: 'info',
+        outfile: 'dist/main.js',
+        platform: 'node',
+        sourcemap: true,
+        watch: process.argv.includes('--watch'),
+        minify: process.argv.includes('--prod'),
+    })
+    .catch(() => process.exit(1));
