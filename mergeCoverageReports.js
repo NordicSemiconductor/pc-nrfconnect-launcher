c3773548a1bb5a0b7e6875f21a156727f2ff5ba6
Svyatoslav <kiev.devel@gmail.com>
2022-10-09 23:42:22 +0300

    Fix merge cov script; set thresholds (low)

--- mergeCoverageReports.js
+++ mergeCoverageReports.js
@@ -7,8 +7,8 @@
 const { execSync } = require('child_process');
 const fs = require('fs-extra');
 
-const REPORTS_FOLDER = 'reports';
-const FINAL_OUTPUT_FOLDER = 'coverage';
+const REPORTS_FOLDER = 'launcher-test-reports';
+const FINAL_OUTPUT_FOLDER = 'launcher-coverage';
 
 const run = commands => {
     commands.forEach(command => execSync(command, { stdio: 'inherit' }));
@@ -26,13 +26,15 @@ fs.copyFileSync(
     `${REPORTS_FOLDER}/from-jest.json`
 );
 
-fs.emptyDirSync('.nyc_output');
+// fs.emptyDirSync('.nyc_output');
 fs.emptyDirSync(FINAL_OUTPUT_FOLDER);
 
 // Run "nyc merge" inside the reports folder, merging the two coverage files into one,
 // then generate the final report on the coverage folder
 run([
     // "nyc merge" will create a "coverage.json" file on the root, we move it to .nyc_output
-    `npx nyc merge ${REPORTS_FOLDER} && mv coverage.json .nyc_output/out.json`,
-    `npx nyc report --reporter lcov --report-dir ${FINAL_OUTPUT_FOLDER}`,
+    `npx nyc merge ${REPORTS_FOLDER}`,
+    `mv coverage.json ${FINAL_OUTPUT_FOLDER}/coverage.json`,
+    // will fail if coverage threshold not met
+    `npx nyc report --reporter text --report-dir ${FINAL_OUTPUT_FOLDER}`,
 ]);
