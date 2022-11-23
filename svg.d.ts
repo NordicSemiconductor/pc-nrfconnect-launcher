462397fc4e1a18daf9a21b469398c81f8a6acfef
Jonas Fritzke Emdal <jonas.emdal@nordicsemi.no>
2022-11-23 16:08:41 +0100

    Lint and other checks.

---
+++ svg.d.ts
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2015 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+ */
+
+declare module '*.svg' {
+    const url: string;
+    export default url;
+}
