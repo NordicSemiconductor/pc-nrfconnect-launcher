f082de6fc4bb4f55518b2f4a26cbda07f2095eff
Jonas <jonas.emdal@nordicsemi.no>
2022-12-08 11:48:56 +0100

    Add serialport dependency

--- src/app/module-loader.js
+++ src/app/module-loader.js
@@ -4,16 +4,11 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
-/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-underscore-dangle,dot-notation */
 import Module from 'module';
 
 const hostedModules = {};
 
-/*
- * The loaded app may import react and react-redux. We must make sure that the
- * app uses the same instances of react and react-redux as we have in core.
- * Cannot have multiple copies of these loaded at the same time.
- */
 const originalLoad = Module._load;
 Module._load = function load(modulePath) {
     if (hostedModules[modulePath]) {
@@ -23,40 +18,9 @@ Module._load = function load(modulePath) {
     return originalLoad.apply(this, arguments); // eslint-disable-line prefer-rest-params
 };
 
-const SerialPort = require('serialport');
-
-let hasShownDeprecatedPropertyWarning = false;
-const mayShowWarningAboutDeprecatedProperty = () => {
-    if (!hasShownDeprecatedPropertyWarning) {
-        console.warn(
-            'Using the property "comName" has been deprecated. You should now use "path". The property will be removed in the next major release.'
-        );
-    }
-    hasShownDeprecatedPropertyWarning = true;
-};
-const ducktapeComName = port => ({
-    ...port,
-    get comName() {
-        mayShowWarningAboutDeprecatedProperty();
-        return port.path;
-    },
-});
-
-const originalSerialPortList = SerialPort.list;
-SerialPort.list = () =>
-    originalSerialPortList().then(ports => ports.map(ducktapeComName));
-
-/* eslint-disable dot-notation */
-// Disable dot-notation in this file, to keep the syntax below more consistent
-hostedModules['serialport'] = SerialPort;
-
+hostedModules['serialport'] = require('serialport');
 hostedModules['electron'] = require('electron');
 hostedModules['@electron/remote'] = require('@electron/remote');
 hostedModules[
     '@nordicsemiconductor/nrf-device-lib-js'
 ] = require('@nordicsemiconductor/nrf-device-lib-js');
-hostedModules['react-dom'] = require('react-dom');
-hostedModules['react-redux'] = require('react-redux');
-hostedModules['react'] = require('react');
-hostedModules['redux-devtools-extension'] = require('redux-devtools-extension');
-hostedModules['redux-thunk'] = require('redux-thunk');
