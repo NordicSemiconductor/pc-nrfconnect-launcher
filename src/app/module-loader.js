d1926ee005f66652de995924fa9f9280770a87ee
Jonas <jonas.emdal@nordicsemi.no>
2022-12-08 14:04:48 +0100

    Bundle react

--- src/app/module-loader.js
+++ src/app/module-loader.js
@@ -4,11 +4,16 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
-/* eslint-disable no-underscore-dangle,dot-notation */
+/* eslint-disable no-underscore-dangle */
 import Module from 'module';
 
 const hostedModules = {};
 
+/*
+ * The loaded app may import react and react-redux. We must make sure that the
+ * app uses the same instances of react and react-redux as we have in core.
+ * Cannot have multiple copies of these loaded at the same time.
+ */
 const originalLoad = Module._load;
 Module._load = function load(modulePath) {
     if (hostedModules[modulePath]) {
@@ -18,9 +23,18 @@ Module._load = function load(modulePath) {
     return originalLoad.apply(this, arguments); // eslint-disable-line prefer-rest-params
 };
 
+/* eslint-disable dot-notation */
+// Disable dot-notation in this file, to keep the syntax below more consistent
 hostedModules['serialport'] = require('serialport');
+
 hostedModules['electron'] = require('electron');
 hostedModules['@electron/remote'] = require('@electron/remote');
 hostedModules[
     '@nordicsemiconductor/nrf-device-lib-js'
 ] = require('@nordicsemiconductor/nrf-device-lib-js');
+hostedModules['pc-nrfconnect-shared'] = require('pc-nrfconnect-shared');
+hostedModules['react-dom'] = require('react-dom');
+hostedModules['react-redux'] = require('react-redux');
+hostedModules['react'] = require('react');
+hostedModules['redux-devtools-extension'] = require('redux-devtools-extension');
+hostedModules['redux-thunk'] = require('redux-thunk');
