a1aa8c8f003b9ca0861b3b57060a41b744bcc957
Pascal Pickel <Pascal.Pickel@nordicsemi.no>
2023-01-05 12:31:45 +0100

    fix: correct minimal app version for tracecollector-preview
    Signed-off-by: Pascal Pickel <Pascal.Pickel@nordicsemi.no>

--- src/launcher/util/minimalRequiredAppVersions.ts
+++ src/launcher/util/minimalRequiredAppVersions.ts
@@ -9,7 +9,7 @@ export default {
     'pc-nrfconnect-linkmonitor': '2.0.3',
     'pc-nrfconnect-ppk': '3.5.4',
     'pc-nrfconnect-rssi': '1.4.3',
-    'pc-nrfconnect-tracecollector-preview': '0.4.0',
+    'pc-nrfconnect-tracecollector-preview': '0.3.7',
     'pc-nrfconnect-tracecollector': '1.1.4',
     'pc-nrfconnect-programmer': '3.0.5',
 } as Record<string, string>;
