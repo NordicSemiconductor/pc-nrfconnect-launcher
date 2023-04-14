d493c95eacb59b405f76e88a5700f814830ace9a
Pascal Pickel <Pascal.Pickel@nordicsemi.no>
2023-04-14 13:45:47 +0200

    feat: add hot-reloading for apps
    This is done through filewatchers watching the bundle.js and bundle.css.
    A change to the bundle.js remounts the `App` component.
    A change in the bundle.css simply updates the stylesheet node with a new
    querystring to avoid reusing cached versions.
    
    Signed-off-by: Pascal Pickel <Pascal.Pickel@nordicsemi.no>

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -10,6 +10,7 @@ import './module-loader';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { dialog } from '@electron/remote';
+import nodeWatch from 'node-watch';
 
 import initApp from './initApp';
 
@@ -20,6 +21,15 @@ const render = App => {
     ReactDOM.render(<App />, document.getElementById('webapp'));
 };
 
+const mountApp = () => {
+    try {
+        const app = initApp(appPath);
+        render(app);
+    } catch (error) {
+        showLoadingError(error);
+    }
+};
+
 const showLoadingError = error => {
     console.error(error);
     dialog.showMessageBox({
@@ -28,9 +38,21 @@ const showLoadingError = error => {
     });
 };
 
-try {
-    const app = initApp(appPath);
-    render(app);
-} catch (error) {
-    showLoadingError(error);
+mountApp();
+
+if (process.env.NODE_ENV === 'development') {
+    nodeWatch(`${appPath}/dist/bundle.js`, evt => {
+        console.log('Hot reloading app due to bundle.js changes', evt);
+        mountApp();
+    });
+
+    nodeWatch(`${appPath}/dist/bundle.css`, () => {
+        console.log('Hot reloading app due to bundle.css changes');
+        const cssNode = [...document.head.children].find(node =>
+            node.href?.includes('bundle.css')
+        );
+        if (cssNode) {
+            cssNode.href = `bundle.css?${Date.now()}`;
+        }
+    });
 }
