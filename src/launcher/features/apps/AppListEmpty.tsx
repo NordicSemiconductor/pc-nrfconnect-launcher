97e433b7d6fc6932e45656ea32086640eff1955b
Marko Schulz <Marko.Schulz@nordicsemi.no>
2023-08-31 13:08:31 +0200

    Refactor: Split up empty app list cases

--- src/launcher/features/apps/AppListEmpty.tsx
+++ src/launcher/features/apps/AppListEmpty.tsx
@@ -4,36 +4,43 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
-import React, { useEffect, useState } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 
 import { useLauncherSelector } from '../../util/hooks';
 import Link from '../../util/Link';
 import { getNoAppsExist } from './appsSlice';
 
-export default () => {
-    const noAppsExist = useLauncherSelector(getNoAppsExist);
+const Box = ({ children }: { children: ReactNode }) => (
+    <div className="tw-grid tw-flex-1 tw-place-items-center">
+        <div className="tw-max-w-[75%] tw-bg-white tw-p-4">{children}</div>
+    </div>
+);
 
+const NotLoadedYet = () => {
     const [justStarted, setJustStarted] = useState(true);
 
     useEffect(() => {
         setTimeout(() => setJustStarted(false), 2000);
     }, []);
 
-    if (justStarted && noAppsExist) return null;
-
-    return (
-        <div className="tw-grid tw-flex-1 tw-place-items-center">
-            <div className="tw-max-w-[75%] tw-bg-white tw-p-4">
-                {noAppsExist ? (
-                    <>
-                        The list of apps is not yet loaded from{' '}
-                        <Link href="https://developer.nordicsemi.com" />. Make
-                        sure you can reach that server.
-                    </>
-                ) : (
-                    'No apps shown because of the selected filters. Change those to display apps again.'
-                )}
-            </div>
-        </div>
+    return justStarted ? null : (
+        <Box>
+            The list of apps is not yet loaded from{' '}
+            <Link href="https://developer.nordicsemi.com" />. Make sure you can
+            reach that server.
+        </Box>
     );
+};
+
+const AllFilteredOut = () => (
+    <Box>
+        No apps shown because of the selected filters. Change those to display
+        apps again.
+    </Box>
+);
+
+export default () => {
+    const noAppsExist = useLauncherSelector(getNoAppsExist);
+
+    return noAppsExist ? <NotLoadedYet /> : <AllFilteredOut />;
 };
