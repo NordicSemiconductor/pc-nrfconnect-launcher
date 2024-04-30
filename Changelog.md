df55fa0e64fb1a3284bcd80c4cbcebc369fce0ef
kylebonnici <kylebonnici@hotmail.com>
2024-04-30 10:42:46 +0200

    Feedback

--- Changelog.md
+++ Changelog.md
@@ -1,4 +1,3 @@
-
 ## 5.0.0 - Unreleased
 
 ### Removed
--- src/main/initNrfUtilProxyEnv.ts
+++ src/main/initNrfUtilProxyEnv.ts
@@ -8,13 +8,8 @@ import argv from './argv';
 
 export default () => {
     const proxyServer = argv['proxy-server'] as string | undefined;
-    const httpProxy = proxyServer
-        ?.split(';')
-        .find(p => p.startsWith('http='))
-        ?.split('=')[1];
-
-    if (httpProxy) {
-        const proxy = `http://${argv['proxy-server']}`;
+    if (proxyServer) {
+        const proxy = `http://${proxyServer}`;
 
         process.env.HTTP_PROXY = proxy;
         process.env.HTTPS_PROXY = proxy;
