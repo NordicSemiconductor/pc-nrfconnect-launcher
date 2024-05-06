94abe5ebeb10dec4e1f71316089ae43dd9125ac5
Sviatoslav <kiev.devel@gmail.com>
2024-05-06 12:17:35 +0200

    Add licence

--- src/launcher/util/formatTimestamp.ts
+++ src/launcher/util/formatTimestamp.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+ */
+
 export default function formatPublishTimestamp(publishTimestamp?: string) {
     if (!publishTimestamp) return '';
 
