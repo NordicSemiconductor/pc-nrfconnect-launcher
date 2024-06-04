650ab374a5ec415a31a69153fcc24c2d253fcb08
Chun Fan <4866953+chunfantasy@users.noreply.github.com>
2024-06-04 15:52:08 +0200

    Update the actions

--- build/winSign.js
+++ build/winSign.js
@@ -4,22 +4,21 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
-const { exec } = require('child_process');
+const { execSync } = require('child_process');
 
 exports.default = configuration => {
     console.log('Start code signing');
     const keypairAlias = process.env.NORDIC_SM_KEYPAIR_ALIAS;
 
     console.log(`Configuration path ${configuration.path}`);
-    console.log(`Env variables ${JSON.stringify(process.env)}`);
+    console.log(`Env variables ${process.env.NORDIC_SM_KEYPAIR_ALIAS}`);
+    console.log(`Env variables ${keypairAlias}`);
     if (configuration.path) {
-        exec(
+        const result = execSync(
             `smctl sign --keypair-alias=${keypairAlias} --input "${String(
                 configuration.path
-            )}"`,
-            error => {
-                console.log(error);
-            }
+            )}"`
         );
+        console.log(`Signing result ${result}`);
     }
 };
