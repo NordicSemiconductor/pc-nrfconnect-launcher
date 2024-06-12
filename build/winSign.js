981f9fc6b12987426d8809fdafb74dae40f847e7
Chun Fan <4866953+chunfantasy@users.noreply.github.com>
2024-06-12 10:27:48 +0200

    Update according to review

--- build/winSign.js
+++ build/winSign.js
@@ -7,31 +7,33 @@
 const { execSync } = require('child_process');
 
 exports.default = configuration => {
+    if (configuration.path) {
+        console.info('Skip signing because configuration.path is undefined');
+    }
+
     if (!process.env.SM_API_KEY) {
-        console.info(`Skip signing because SM_API_KEY is not configured`);
+        console.info('Skip signing because SM_API_KEY is not configured');
         return;
     }
     if (!process.env.SM_KEYPAIR_ALIAS) {
-        console.info(`Skip signing because SM_KEYPAIR_ALIAS is not configured`);
+        console.info('Skip signing because SM_KEYPAIR_ALIAS is not configured');
         return;
     }
 
     console.log('Start code signing');
     const keypairAlias = process.env.SM_KEYPAIR_ALIAS;
 
-    if (configuration.path) {
-        try {
-            const output = execSync(
-                `smctl sign --keypair-alias="${keypairAlias}" --input "${String(
-                    configuration.path
-                )}"  --verbose`,
-                {
-                    stdio: 'inherit',
-                }
-            );
-            console.log(`Signing succeeded: ${output}`);
-        } catch (e) {
-            console.log(`Signing failed with error: ${e}`);
-        }
+    try {
+        const output = execSync(
+            `smctl sign --keypair-alias="${keypairAlias}" --input "${String(
+                configuration.path
+            )}"  --verbose`,
+            {
+                stdio: 'inherit',
+            }
+        );
+        console.log(`Signing succeeded: ${output}`);
+    } catch (e) {
+        console.log(`Signing failed with error: ${e}`);
     }
 };
