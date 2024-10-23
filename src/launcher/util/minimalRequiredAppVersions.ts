18d0f7d231b23148c3e1b2cb1ea6a9ee022b1908
Sviatoslav <kiev.devel@gmail.com>
2024-10-23 16:51:43 +0200

    Update app version; update min app versions required

--- src/launcher/util/minimalRequiredAppVersions.ts
+++ src/launcher/util/minimalRequiredAppVersions.ts
@@ -8,14 +8,14 @@ export default {
     'pc-nrfconnect-cellularmonitor':
         process.platform === 'darwin' && process.arch !== 'x64'
             ? '2.4.0'
-            : '1.0.3',
+            : '2.0.0',
     'pc-nrfconnect-dtm': '2.2.0',
     'pc-nrfconnect-npm': '1.0.0',
     'pc-nrfconnect-ppk': '4.0.0',
-    'pc-nrfconnect-programmer': '4.0.0',
+    'pc-nrfconnect-programmer': '4.4.2',
     'pc-nrfconnect-rssi': '1.5.0',
     'pc-nrfconnect-serial-terminal': '1.2.0',
-    'pc-nrfconnect-toolchain-manager': '1.3.0',
+    'pc-nrfconnect-toolchain-manager': '1.5.3',
     'pc-nrfconnect-gettingstarted': null,
     'pc-nrfconnect-linkmonitor': null,
     'pc-nrfconnect-tracecollector-preview': null,
