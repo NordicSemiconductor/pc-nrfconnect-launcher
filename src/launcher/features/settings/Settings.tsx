3e7724ed2b30d3d565963701b6237872c6e3908a
Marko Schulz <Marko.Schulz@nordicsemi.no>
2024-12-13 14:46:25 +0100

    Split up Settings component

--- src/launcher/features/settings/Settings.tsx
+++ src/launcher/features/settings/Settings.tsx
@@ -39,161 +39,172 @@ import UpdateCheckCompleteDialog from './UpdateCheckCompleteDialog';
 
 const { white, gray700, nordicBlue } = colors;
 
-export default () => {
+const Updates = () => {
     const dispatch = useLauncherDispatch();
 
     const shouldCheckForUpdatesAtStartup = useLauncherSelector(
         getShouldCheckForUpdatesAtStartup
     );
-    const isSendingTelemetry = useLauncherSelector(getIsSendingTelemetry);
-
-    const sources = useLauncherSelector(getSources);
 
     const { isCheckingForUpdates, lastUpdateCheckDate } =
         useLauncherSelector(getUpdateCheckStatus);
 
     return (
-        <WithScrollbarContainer>
-            <div className="settings-pane-container">
-                <Card body>
-                    <Row>
-                        <Col>
-                            <Card.Title>Updates</Card.Title>
+        <Card body>
+            <Row>
+                <Col>
+                    <Card.Title>Updates</Card.Title>
+                </Col>
+                <Col xs="auto">
+                    <Button
+                        variant="outline-primary"
+                        onClick={() => dispatch(checkForUpdatesManually())}
+                        disabled={isCheckingForUpdates}
+                    >
+                        {isCheckingForUpdates
+                            ? 'Checking...'
+                            : 'Check for updates'}
+                    </Button>
+                </Col>
+            </Row>
+            <p className="small text-muted">
+                {lastUpdateCheckDate != null && (
+                    <>
+                        Last update check performed:{' '}
+                        {formatDate(lastUpdateCheckDate, 'yyyy-MM-dd HH:mm:ss')}
+                    </>
+                )}
+            </p>
+            <Toggle
+                id="checkForUpdates"
+                label="Check for updates at startup"
+                onToggle={() =>
+                    dispatch(
+                        setCheckForUpdatesAtStartup(
+                            !shouldCheckForUpdatesAtStartup
+                        )
+                    )
+                }
+                isToggled={shouldCheckForUpdatesAtStartup}
+                variant="primary"
+                handleColor={white}
+                barColor={gray700}
+                barColorToggled={nordicBlue}
+            />
+        </Card>
+    );
+};
+
+const Sources = () => {
+    const dispatch = useLauncherDispatch();
+
+    const sources = useLauncherSelector(getSources);
+
+    return (
+        <Card body id="app-sources">
+            <Row>
+                <Col>
+                    <Card.Title>App sources</Card.Title>
+                </Col>
+                <Col xs="auto">
+                    <Button
+                        variant="outline-primary"
+                        onClick={() => dispatch(showAddSource())}
+                    >
+                        Add source
+                    </Button>
+                </Col>
+            </Row>
+            {sources
+                .filter(source => source.name !== OFFICIAL)
+                .map(source => (
+                    <Row key={source.name}>
+                        <Col className="item-name text-capitalize">
+                            {source.name}
                         </Col>
                         <Col xs="auto">
-                            <Button
-                                variant="outline-primary"
-                                onClick={() =>
-                                    dispatch(checkForUpdatesManually())
-                                }
-                                disabled={isCheckingForUpdates}
-                            >
-                                {isCheckingForUpdates
-                                    ? 'Checking...'
-                                    : 'Check for updates'}
-                            </Button>
+                            <ButtonToolbar>
+                                <Button
+                                    variant="outline-secondary"
+                                    size="sm"
+                                    onClick={() =>
+                                        clipboard.writeText(
+                                            source.url.replace(
+                                                /source\.json$/,
+                                                'apps.json'
+                                            )
+                                        )
+                                    }
+                                    title="Copy URL to clipboard"
+                                >
+                                    Copy URL
+                                </Button>
+                                <Button
+                                    variant="outline-secondary"
+                                    size="sm"
+                                    onClick={() =>
+                                        dispatch(showRemoveSource(source.name))
+                                    }
+                                    title="Remove source and associated apps"
+                                >
+                                    Remove
+                                </Button>
+                            </ButtonToolbar>
                         </Col>
                     </Row>
-                    <p className="small text-muted">
-                        {lastUpdateCheckDate != null && (
-                            <>
-                                Last update check performed:{' '}
-                                {formatDate(
-                                    lastUpdateCheckDate,
-                                    'yyyy-MM-dd HH:mm:ss'
-                                )}
-                            </>
-                        )}
-                    </p>
+                ))}
+        </Card>
+    );
+};
+
+const UsageStatistics = () => {
+    const dispatch = useLauncherDispatch();
+
+    const isSendingTelemetry = useLauncherSelector(getIsSendingTelemetry);
+
+    return (
+        <Card body>
+            <Row>
+                <Col>
+                    <Card.Title>Usage statistics</Card.Title>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
                     <Toggle
-                        id="checkForUpdates"
-                        label="Check for updates at startup"
-                        onToggle={() =>
-                            dispatch(
-                                setCheckForUpdatesAtStartup(
-                                    !shouldCheckForUpdatesAtStartup
-                                )
-                            )
-                        }
-                        isToggled={shouldCheckForUpdatesAtStartup}
+                        id="checkForShare"
+                        label="Collect anonymous usage data"
+                        onToggle={() => dispatch(toggleSendingTelemetry())}
+                        isToggled={isSendingTelemetry}
                         variant="primary"
                         handleColor={white}
                         barColor={gray700}
                         barColorToggled={nordicBlue}
                     />
-                </Card>
-                <Card body id="app-sources">
-                    <Row>
-                        <Col>
-                            <Card.Title>App sources</Card.Title>
-                        </Col>
-                        <Col xs="auto">
-                            <Button
-                                variant="outline-primary"
-                                onClick={() => dispatch(showAddSource())}
-                            >
-                                Add source
-                            </Button>
-                        </Col>
-                    </Row>
-                    {sources
-                        .filter(source => source.name !== OFFICIAL)
-                        .map(source => (
-                            <Row key={source.name}>
-                                <Col className="item-name text-capitalize">
-                                    {source.name}
-                                </Col>
-                                <Col xs="auto">
-                                    <ButtonToolbar>
-                                        <Button
-                                            variant="outline-secondary"
-                                            size="sm"
-                                            onClick={() =>
-                                                clipboard.writeText(
-                                                    source.url.replace(
-                                                        /source\.json$/,
-                                                        'apps.json'
-                                                    )
-                                                )
-                                            }
-                                            title="Copy URL to clipboard"
-                                        >
-                                            Copy URL
-                                        </Button>
-                                        <Button
-                                            variant="outline-secondary"
-                                            size="sm"
-                                            onClick={() =>
-                                                dispatch(
-                                                    showRemoveSource(
-                                                        source.name
-                                                    )
-                                                )
-                                            }
-                                            title="Remove source and associated apps"
-                                        >
-                                            Remove
-                                        </Button>
-                                    </ButtonToolbar>
-                                </Col>
-                            </Row>
-                        ))}
-                </Card>
-                <Card body>
-                    <Row>
-                        <Col>
-                            <Card.Title>Usage statistics</Card.Title>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <Toggle
-                                id="checkForShare"
-                                label="Collect anonymous usage data"
-                                onToggle={() =>
-                                    dispatch(toggleSendingTelemetry())
-                                }
-                                isToggled={isSendingTelemetry}
-                                variant="primary"
-                                handleColor={white}
-                                barColor={gray700}
-                                barColorToggled={nordicBlue}
-                            />
-                        </Col>
-                        <Col xs="auto">
-                            <Button
-                                variant="outline-primary"
-                                onClick={() => dispatch(showTelemetryDialog())}
-                            >
-                                Show agreement
-                            </Button>
-                        </Col>
-                    </Row>
-                </Card>
-                <UpdateCheckCompleteDialog />
-                <AddSourceDialog />
-                <ConfirmRemoveSourceDialog />
-            </div>
-        </WithScrollbarContainer>
+                </Col>
+                <Col xs="auto">
+                    <Button
+                        variant="outline-primary"
+                        onClick={() => dispatch(showTelemetryDialog())}
+                    >
+                        Show agreement
+                    </Button>
+                </Col>
+            </Row>
+        </Card>
     );
 };
+
+export default () => (
+    <WithScrollbarContainer>
+        <div className="settings-pane-container">
+            <Updates />
+            <Sources />
+            <UsageStatistics />
+
+            <UpdateCheckCompleteDialog />
+            <AddSourceDialog />
+            <ConfirmRemoveSourceDialog />
+        </div>
+    </WithScrollbarContainer>
+);
