18ad2d79cbef58cd5204e5c64750e5997e411011
Marko Schulz <Marko.Schulz@nordicsemi.no>
2025-01-13 13:17:44 +0100

    Simplify parameters in low-level net functions

--- src/main/apps/apps.ts
+++ src/main/apps/apps.ts
@@ -167,7 +167,7 @@ export const downloadLatestAppInfos = async () => {
 export const downloadAppInfos = async (source: Source) => {
     const downloadableApps = await Promise.all(
         getAppUrls(source).map(async appUrl => {
-            const appInfo = await downloadToJson<AppInfo>(appUrl, true);
+            const appInfo = await downloadToJson<AppInfo>(appUrl);
 
             if (path.basename(appUrl) !== `${appInfo.name}.json`) {
                 inRenderer.showErrorDialog(
