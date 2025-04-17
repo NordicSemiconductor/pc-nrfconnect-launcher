063698f066282708d019ee207364a7c4b08025e4
Marko Schulz <Marko.Schulz@nordicsemi.no>
2025-04-17 18:01:34 +0200

    Simplify code
    The exception for macOS does not seem to be necessary anymore.

--- src/main/open.ts
+++ src/main/open.ts
@@ -4,27 +4,15 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
-import childProcess from 'child_process';
 import { shell } from 'electron';
 import fs from 'fs';
-import os from 'os';
 
 export const openFile = (filePath: string) => {
-    const exists = fs.existsSync(filePath);
-    if (!exists) {
+    if (!fs.existsSync(filePath)) {
         throw new Error(`Could not find file at path: ${filePath}`);
     }
 
-    const escapedPath = filePath.replace(/ /g, '\\ ');
-
-    // Could not find a method that works on all three platforms:
-    // * shell.openItem works on Windows and Linux but not on OSX
-    // * childProcess.execSync works on OSX but not on Windows
-    if (os.type() === 'Darwin') {
-        childProcess.execSync(`open ${escapedPath}`);
-    } else {
-        shell.openPath(filePath);
-    }
+    shell.openPath(filePath);
 };
 
 export const openUrl = (url: string) => {
