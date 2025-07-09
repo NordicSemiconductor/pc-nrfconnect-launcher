5a0fc6c3fb9d9e95f6b538c4c154fb25327305f5
Mia Pickel <Mia.Pickel@nordicsemi.no>
2025-07-09 14:28:59 +0200

    feat: use jlink download from new library

---
+++ src/main/bundledJlink.js
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2015 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+ */
+
+const path = require('path');
+
+exports.bundledJlinkVersion = 'V8.18';
+exports.bundledJlinkDir = path.join('prefetched', 'jlink');
