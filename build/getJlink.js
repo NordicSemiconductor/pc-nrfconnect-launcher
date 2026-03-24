5c97be181090c8fbcc65b3c5aa97d8c797c58bf1
Marko Schulz <Marko.Schulz@nordicsemi.no>
2026-03-24 15:47:46 +0100

    Allow overriding the download URL for J-Link

--- build/getJlink.js
+++ build/getJlink.js
@@ -5,13 +5,41 @@
  */
 
 const path = require('path');
+const os = require('os');
 
 const downloadJLink =
     require('@nordicsemiconductor/nrf-jlink-js').downloadAndSaveJLink;
 
 const bundledJLink = require('../src/main/bundledJlink');
+const downloadFile = require('../scripts/downloadFile');
 
-exports.default = () =>
+const downloadSpecificJLink = version => {
+    const platform = {
+        win32: 'Windows',
+        darwin: 'MacOSX',
+        linux: 'Linux',
+    }[os.platform()];
+    const arch = os.arch() === 'x64' ? 'x86_64' : os.arch();
+    const fileExtension = {
+        win32: 'exe',
+        darwin: 'pkg',
+        linux: 'deb',
+    }[os.platform()];
+
+    const filename = `JLink_${platform}_V${version.replace('.', '').replace(/^v/, '')}_${arch}.${fileExtension}`;
+
+    const url = `https://files.nordicsemi.com/ui/api/v1/download?isNativeBrowsing=true&repoKey=swtools&path=external/ncd/jlink/${filename}`;
+
+    return downloadFile(
+        url,
+        path.join('resources', 'prefetched', 'jlink', filename),
+    ).catch(error => {
+        console.error(`Failed to download J-Link from ${url}: `, error.message);
+        process.exit(-1);
+    });
+};
+
+const downloadLatestJLink = () =>
     downloadJLink(path.join('resources', 'prefetched', 'jlink'))
         .then(result => {
             if (bundledJLink.toLowerCase() !== result.version.toLowerCase()) {
@@ -20,8 +48,17 @@ exports.default = () =>
                 );
                 process.exit(-1);
             }
+
+            console.log(
+                `Downloaded J-Link version ${result.version} successfully.`,
+            );
         })
         .catch(error => {
             console.error('\n!!! EXCEPTION', error.message);
             process.exit(-1);
         });
+
+exports.default = () =>
+    process.env.OVERRIDE_JLINK_VERSION
+        ? downloadSpecificJLink(process.env.OVERRIDE_JLINK_VERSION)
+        : downloadLatestJLink();
